490227f52a2106ea5eb38d598db6f56f33a7c7d0
sergei.milev <sergei.milev@limechain.tech>
2025-07-15 16:47:08 +0300

    Refactor deploy script and add mock provider implementation
    - Updated `deploy.sh` to include mainnet RPC URL and mock provider deployment logic.
    - Added new `mock-provider` module with basic functionality and environment configuration.
    - Introduced interfaces for `IFriStatementVerifier` and `IMerkleStatementVerifier` in the Rust code.
    - Enhanced `FriStatementVerifier` with new verification logic and improved function signatures for clarity.

--- stylus/deploy.sh
+++ stylus/deploy.sh
@@ -1,28 +1,17 @@
 #! /bin/bash
-# Deployment order:
-# - Auxiliary contracts:
-#  - pedersen-hp-x-c
-#  - pedersen-hp-y-c
-#  - poseidon-frk-0-col
-#  - poseidon-frk-1-col
-#  - poseidon-frk-2-col
-#  - poseidon-prk-0-col
-#  - poseidon-prk-1-col
-# - Computation contracts:
-#  - oods
-#  - constraint-poly-preparer
-#  - constraint-poly-finalizer
-#  - constraint-poly
-# - Main contracts ZK logic contracts:
-#  - memory-page-fact-registry
-#  - cpu
-#  - gps
-# set -euo pipefail
-
-RPC_URL="http://127.0.0.1:8547"
+RPC_URL_LOCAL="http://127.0.0.1:8547"
+RPC_URL_MAINNET="https://mainnet.arbitrum.io/rpc"
+RPC_URL=$RPC_URL_LOCAL
 TIMEOUT=2
 PK="0xb6b15c8cb491557369f3c7d2c287b053eb229daa9c22138887752191c9520659"
+
+# Mainnet addresses
+fri_statement_verifier_address="0x30EfaAA99f8eFe310D9FdC83072e2a04c093d400"
+merkle_statement_verifier_address="0x32a91Ff604AB2aDCd832e91D68b2f3f25358FdAd"
+
+# Deployment order:
 contracts=(
+# - Auxiliary contracts:
   pedersen-hp-x-c
   pedersen-hp-y-c
   poseidon-frk-0-col
@@ -30,12 +19,16 @@ contracts=(
   poseidon-frk-2-col
   poseidon-prk-0-col
   poseidon-prk-1-col
+  verifier-init
+# - Computation contracts:
   oods
   constraint-poly-preparer
   constraint-poly-finalizer
   constraint-poly
-  mpfr
-#   cpu
+    fri-statement-verifier
+# - Main contracts ZK logic contracts:
+#   mpfr
+  cpu-verifier
 #   gps
 )
 
@@ -77,10 +70,17 @@ function check_dependencies() {
 function deploy_contract() {
     local name=$1
     local address
+
     pushd "stylus/$name"
     echo "Deploying '$name' contract from $(pwd)..."
     local var_name="${name//-/_}_address"
-    DEPLOY_OUT=$(cargo stylus deploy  --private-key=$PK --endpoint=$RPC_URL 2>&1)
+    local deploy_args=(--private-key="$PK" --endpoint="$RPC_URL")
+    if [ "$name" == "cpu-verifier" ] \
+    || [ "$name" == "verifier-init" ] \
+    || [ "$name" == "fri-statement-verifier" ]; then
+        deploy_args+=(--no-verify)
+    fi
+    DEPLOY_OUT=$(cargo stylus deploy "${deploy_args[@]}" 2>&1)
     address=$(echo "$DEPLOY_OUT" | grep -i 'deployed code at address' | grep -Eo '0x[a-fA-F0-9]{40}')
     if [ -z "$address" ]; then
         echo "❌ Deployment failed for '$name' contract"
@@ -93,24 +93,93 @@ function deploy_contract() {
     popd > /dev/null
 }
 
+function deploy_contract_estimate_gas() {
+    local name=$1
+    local address
+    pushd "stylus/$name"
+    echo "Deploying '$name' contract from $(pwd)..."
+    local var_name="${name//-/_}_address"
+    DEPLOY_OUT=$(cargo stylus deploy  --estimate-gas --private-key=$PK --endpoint=$RPC_URL 2>&1)
+    gas=$(echo "$DEPLOY_OUT" | grep -i 'deployment tx gas: ' )
+    if [ -z "$gas" ]; then
+        echo "❌ Deployment failed for '$name' contract"
+        echo "---- Last 20 lines of output for debugging: ----"
+        echo "$DEPLOY_OUT" | tail -20
+        exit 1
+    fi
+    echo "✅ '$name': $gas"
+    # eval "$var_name=$address"
+    popd > /dev/null
+}
+
 # --- Deploy contracts
 check_devnode
 check_dependencies
+
+
+# If local, deploy mock-provider and use it for the verifiers
+if [ "$RPC_URL" == "$RPC_URL_LOCAL" ]; then
+    deploy_contract "mock-provider"
+    merkle_statement_verifier_address=$mock_provider_address
+    fri_statement_verifier_address=$mock_provider_address
+    echo "Using mock providers for the verifier"
+    echo "✅ Merkle statement contract address: $merkle_statement_verifier_address"
+    echo "✅ Fri statement contract address: $fri_statement_verifier_address"
+fi
+
 
 for name in "${contracts[@]}"; do
     deploy_contract "$name"
     var_name="${name//-/_}_address"
     eval "echo \"Captured address: \$$var_name\""
+    if [ "$name" == "constraint-poly" ]; then
+        echo "Setting addresses on $name via cast send..."
+        CAST_OUT=$(cast send $constraint_poly_address "setAddresses(address,address)" \
+            $constraint_poly_preparer_address $constraint_poly_finalizer_address \
+            --rpc-url=$RPC_URL --private-key=$PK | grep "1 (success)")
+        if [ -z "$CAST_OUT" ]; then
+            echo "❌ Failed to call setAddresses on $constraint_poly_address"
+            exit 1
+        fi
+        echo "✅ Successfully set addresses on $constraint_poly_address"
+    elif [ "$name" == "fri-statement-verifier" ]; then
+        echo "Setting addresses on $name via cast send..."
+        CAST_OUT=$(cast send $fri_statement_verifier_address "init(address,address,address)" \
+            $oods_address \
+            $fri_statement_verifier_address \
+            $merkle_statement_verifier_address \
+            --rpc-url=$RPC_URL --private-key=$PK | grep "1 (success)")
+        if [ -z "$CAST_OUT" ]; then
+            echo "❌ Failed to call init on $fri_statement_verifier_address"
+            exit 1
+        fi
+        echo "✅ Successfully set addresses on $fri_statement_verifier_address"
+    elif [ "$name" == "cpu-verifier" ]; then
+        echo "Setting addresses on $name via cast send..."
+        CAST_OUT=$(cast send $cpu_verifier_address \
+            "init(address,address,address,address,address,address,address,address,address,address)" \
+            $constraint_poly_address \
+            $pedersen_hp_x_c_address \
+            $pedersen_hp_y_c_address \
+            $poseidon_frk_0_col_address \
+            $poseidon_frk_1_col_address \
+            $poseidon_frk_2_col_address \
+            $poseidon_prk_0_col_address \
+            $poseidon_prk_1_col_address \
+            $verifier_init_address \
+            $fri_statement_verifier_address \
+            --rpc-url=$RPC_URL --private-key=$PK | grep "1 (success)")
+        if [ -z "$CAST_OUT" ]; then
+            echo "❌ Failed to call init on $cpu_verifier_address"
+            exit 1
+        fi
+        echo "✅ Successfully set addresses on $cpu_verifier_address"
+    fi
 done
 
-# --- SetUp contracts
-echo "Setting addresses on constraint-poly via cast send..."
-CAST_OUT=$(cast send $constraint_poly_address "setAddresses(address,address)" \
-    $constraint_poly_preparer_address $constraint_poly_finalizer_address \
-    --rpc-url=$RPC_URL --private-key=$PK | grep "1 (success)")
+# for name in "${contracts[@]}"; do
+#     deploy_contract_estimate_gas "$name"
+#     # var_name="${name//-/_}_address"
+#     # eval "echo \"Captured address: \$$var_name\""
+# done
 
-if [ -z "$CAST_OUT" ]; then
-    echo "❌ Failed to call setAddresses on $constraint_poly_address"
-    exit 1
-fi
-echo "✅ Successfully set addresses on $constraint_poly_address"
--- stylus/fri-statement-verifier/src/interfaces.rs
+++ stylus/fri-statement-verifier/src/interfaces.rs
@@ -1,6 +1,14 @@
 use stylus_sdk::prelude::sol_interface;
 
 sol_interface! {
+    interface IFriStatementVerifier {
+        function isValid(bytes32 fact) external view returns (bool);
+    }
+
+    interface IMerkleStatementVerifier {
+        function isValid(bytes32 fact) external view returns (bool);
+    }
+
     interface ICpuOods {
         function compute(uint256[] memory ctx) external view returns(uint256[] memory);
     }
